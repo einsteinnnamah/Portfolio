5a635dc5b64cdb1c9b897d356a1b30966e967e5e
einsteinnnamah <einsteinnnamah@gmail.com>
2023-05-17 12:39:35 +0100

    added header for the jamb casestudy, improving accessiblity too

--- src/Pages/Casestudies/Jamb.jsx
+++ src/Pages/Casestudies/Jamb.jsx
@@ -6,10 +6,16 @@ const Jamb = () => {
     <>
     <Header />
     <div  className="xl:w-[1440px] px-[24px]  xl:px-[120px] lg:px-[64px] md:px-[32px] m-auto">
-     
       <div className='mt-[64px]'>
-        <div className='text-[gray]'><Link to="/" className='text-[#1e1e1e]'>Home</Link>/ <Link to="/" className='text-[#1e1e1e]'>Works</Link> / Jamb</div>
-        <h1>more update by tomorrow</h1>
+        <div className='text-[gray] font-Nunito text-[24px]'><Link to="/" className='text-[#1e1e1e]'>Home</Link>/ <Link to="/" className='text-[#1e1e1e]'>Works</Link> / Jamb</div>
+        {/* Header */}
+        <div className='text-[48px] text-center mt-[60px] font-Gambetta font-semibold'>
+        JAMB (Joint Admissions and Matriculation Board) homepage redesign
+        </div>
+
+        <div>
+          
+        </div>
       </div>
     </div>
     </>
